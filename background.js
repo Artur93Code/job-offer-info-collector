1a06733983d4efe114e80bda795940aafea52da5
Artur <artur93.praca@gmail.com>
2023-08-18 22:36:34 +0200

    Improve badge text

--- background.js
+++ background.js
@@ -6,13 +6,13 @@ chrome.runtime.onInstalled.addListener(() => {
 
 const pracujPl = 'https://www.pracuj.pl/praca'
 
-//Change badge text when dedected Pracuj.pl on active tab
+//Change badge text when swith on active tab with Pracuj.pl offer
 chrome.tabs.onActivated.addListener(async (tab) => {
   //get url active tab
   chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
     let url = tabs[0].url;
     //Use `url` here inside the callback because it's asynchronous!
-    if (url.startsWith(pracujPl)) {
+    if (url.startsWith(pracujPl) && url.includes('oferta')) {
       chrome.action.setBadgeText({
         tabId: tab.id,
         text: "ON",
@@ -26,6 +26,30 @@ chrome.tabs.onActivated.addListener(async (tab) => {
 });
 });
 
+//Change badge text active tab is updated. Check new url on active tab
+chrome.tabs.onUpdated.addListener(function
+  (tabId, changeInfo, tab) {
+    if (changeInfo.url) {
+  //get url active tab
+  chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
+    let url = tabs[0].url;
+    //Use `url` here inside the callback because it's asynchronous!
+    if (url.startsWith(pracujPl) && url.includes('oferta')) {
+      chrome.action.setBadgeText({
+        tabId: tab.id,
+        text: "ON",
+      });
+    }else{
+      chrome.action.setBadgeText({
+        tabId: tab.id,
+        text: "OFF",
+      });
+    }
+});
+    }
+});
+
+//get company name, job title and url address from offer
 function getData(url){
   console.log(url);
   var job =  document.querySelector('[data-scroll-id="job-title"]').textContent;
@@ -36,7 +60,7 @@ function getData(url){
 }
 
 chrome.action.onClicked.addListener(async (tab) => {
-  if (tab.url.startsWith(pracujPl)) {
+  if (tab.url.startsWith(pracujPl) && url.includes('oferta')) {
     
     await chrome.scripting.executeScript({
       target: {tabId: tab.id},
